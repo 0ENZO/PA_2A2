5fed89165e21b2903ff7df189b00e37ba6689db3
Akael92 <58888896+Akael92@users.noreply.github.com>
2020-03-17 18:47:10 +0100

    V2 QRCODE

--- Qrcode/main.c
+++ Qrcode/main.c
@@ -8,10 +8,18 @@
 
 void Accueil();
 void AccueilDestroy();
+void inscription();
 
 GtkBuilder *builder;
 
-//GtkWidget *sign;
+GtkWidget *sign;
+
+int main(int argc, char *argv[])
+{
+    gtk_init(&argc, &argv);
+    Accueil();
+    return 0;
+}
 
 void Accueil()
 {
@@ -20,10 +28,20 @@ void Accueil()
     gtk_builder_connect_signals(builder, NULL);
     window  = GTK_WIDGET(gtk_builder_get_object(builder,"windows"));
     gtk_widget_show(window);
-    //sign = GTK_WIDGET(gtk_builder_get_object(builder,"inscription"));
-   // g_signal_connect (sign,"clicked",G_CALLBACK(inscription),window);
-
+    sign = GTK_WIDGET(gtk_builder_get_object(builder,"inscription"));
+    g_signal_connect (sign,"clicked",G_CALLBACK(inscription),window);
     gtk_main();
+}
+
+void inscription(GtkWidget *widget,GtkWidget *window)
+{
+    gtk_widget_destroy(window);
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file(builder,"inscription.glade",NULL);
+    gtk_builder_connect_signals(builder, NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder,"windows_inscription"));
+    gtk_widget_show(window);
 }
 
 void AccueilDestroy(GtkWidget *widget,GtkWidget *window)
@@ -33,14 +51,6 @@ void AccueilDestroy(GtkWidget *widget,GtkWidget *window)
     gtk_builder_connect_signals(builder, NULL);
     window  = GTK_WIDGET(gtk_builder_get_object(builder,"windows"));
     gtk_widget_show(window);
-    //sign = GTK_WIDGET(gtk_builder_get_object(builder,"inscription"));
-    //g_signal_connect (sign,"clicked",G_CALLBACK(inscription),window);
-    gtk_main();
-}
-
-int main(int argc, char *argv[])
-{
-    gtk_init(&argc, &argv);
-    Accueil();
-    return 0;
+    sign = GTK_WIDGET(gtk_builder_get_object(builder,"inscription"));
+    g_signal_connect (sign,"clicked",G_CALLBACK(inscription),window);
 }
