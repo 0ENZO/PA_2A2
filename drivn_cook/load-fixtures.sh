9f6240604abdaf70bdadb538a71a918324b11990
Romain <pierucci.romain@gmail.com>
2020-06-09 11:56:21 +0200

    Correction bug script load_fixtures.sh

--- drivn_cook/load-fixtures.sh
+++ drivn_cook/load-fixtures.sh
@@ -24,4 +24,4 @@ php bin/console doctrine:database:create
 php bin/console doctrine:schema:update --force
 
 # Charge les fixtures
-php bin/console doctrine:fixtures:load --no-interaction
+php bin/console doctrine:fixtures:load --append
