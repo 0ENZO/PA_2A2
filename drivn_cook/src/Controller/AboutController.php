baad17262d3b3760cc19ce49cce1e6d28fb631b2
Akael <aboudon2@myges.fr>
2020-06-12 15:50:45 +0200

    Update alex branch

---
+++ drivn_cook/src/Controller/AboutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class AboutController extends AbstractController
+{
+    /**
+     * @Route("/about", name="about")
+     */
+    public function index(): Response
+    {
+        return $this->render('about/about.html.twig', [
+
+        ]);
+    }
+
+}
