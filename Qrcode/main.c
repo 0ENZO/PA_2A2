c0ed6017da87b1c25edf4e9df7f499f170f87c84
Akael <aboudon2@myges.fr>
2020-07-05 23:35:24 +0200

    Update C program and delete second Qrcode with curl folder

--- Qrcode/main.c
+++ Qrcode/main.c
@@ -3,95 +3,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <memory.h>
 #include <gtk-3.0/gtk/gtk.h>
 #include <gtk-3.0/gtk/gtkx.h>
 #include <SDL2/SDL.h>
-#include <mysql/mysql.h>
 #include <SDL2/SDL_image.h>
 #include <curl/curl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <ctype.h>
 #include "qrcodegen.h"
+#include "sha256.h"
 
-#undef DISABLE_SSH_AGENT
+
+#define LOCAL_FILE      "/home/alex/Bureau/Qrcode/essai.png"
+
 void Accueil();
 void AccueilDestroy();
 void inscription();
 void check();
-void finish_with_error(MYSQL *);
- void printQr(const uint8_t qrcode[]);
+void printQr(const uint8_t qrcode[]);
 void qrEncode(char *);
-int finish();
+int transfert();
 
-void enterImmatriculation1();
-void enterImmatriculation2();
-void enterImmatriculation3();
+
 void enteremail();
-void enterfoodtruck();
-void enterfranchise();
-void entercapital();
-void enterpassword();
+void enterfirstname();
+void enterlastname();
+void enterphonenumber();
+//void enterpassword();
 
 
 GtkBuilder *builder;
 
 GtkWidget *sign;
 GtkWidget *validation;
-GtkWidget *immatriculation1;
-GtkWidget *immatriculation2;
-GtkWidget *immatriculation3;
 GtkWidget *email;
-GtkWidget *foodtruck;
-GtkWidget *franchise;
-GtkWidget *capital;
-GtkWidget *password;
+GtkWidget *firstname;
+GtkWidget *lastname;
+GtkWidget *phonenumber;
+//GtkWidget *password;
 
-const char *immatriculation1char;
-const char *immatriculation2char;
-const char *immatriculation3char;
 const char *emailchar;
-const char *foodtruckchar;
-const char *franchisechar;
-const char *capitalchar;
-const char *passwordchar;
+const char *firstnamechar;
+const char *lastnamechar;
+const char *phonenumberchar;
+//const char *passwordchar;
 
-void enterImmatriculation1(GtkWidget *widget,GtkWidget *entry)
-{
-    immatriculation1char = gtk_entry_get_text(GTK_ENTRY(entry));
-}
 
-void enterImmatriculation2(GtkWidget *widget,GtkWidget *entry)
-{
-    immatriculation2char = gtk_entry_get_text(GTK_ENTRY(entry));
-}
-
-void enterImmatriculation3(GtkWidget *widget,GtkWidget *entry)
-{
-    immatriculation3char = gtk_entry_get_text(GTK_ENTRY(entry));
-}
-
-void enteremail(GtkWidget *widget,GtkWidget *entry)
+void enteremail(GtkWidget *entry)
 {
     emailchar = gtk_entry_get_text(GTK_ENTRY(entry));
 }
 
-void enterfoodtruck(GtkWidget *widget,GtkWidget *entry)
+void enterfirstname(GtkWidget *entry)
 {
-    foodtruckchar = gtk_entry_get_text(GTK_ENTRY(entry));
+    firstnamechar = gtk_entry_get_text(GTK_ENTRY(entry));
 }
 
-void enterfranchise(GtkWidget *widget,GtkWidget *entry)
+void enterlastname(GtkWidget *entry)
 {
-    franchisechar = gtk_entry_get_text(GTK_ENTRY(entry));
+    lastnamechar = gtk_entry_get_text(GTK_ENTRY(entry));
 }
 
-void entercapital(GtkWidget *widget,GtkWidget *entry)
+void enterphonenumber(GtkWidget *entry)
 {
-    capitalchar = gtk_entry_get_text(GTK_ENTRY(entry));
+    phonenumberchar = gtk_entry_get_text(GTK_ENTRY(entry));
 }
 
-void enterpassword(GtkWidget *widget,GtkWidget *entry)
+/*void enterpassword(GtkWidget *entry)
 {
     passwordchar = gtk_entry_get_text(GTK_ENTRY(entry));
-}
+}*/
 
 
 int main(int argc, char *argv[])
@@ -122,30 +105,24 @@ void inscription(GtkWidget *widget,GtkWidget *window)
 
     window = GTK_WIDGET(gtk_builder_get_object(builder,"windows_inscription"));
     gtk_widget_show(window);
-    immatriculation1 = GTK_WIDGET(gtk_builder_get_object(builder,"immatriculation1"));
-    immatriculation2 = GTK_WIDGET(gtk_builder_get_object(builder,"immatriculation2"));
-    immatriculation3 = GTK_WIDGET(gtk_builder_get_object(builder,"immatriculation3"));
     email = GTK_WIDGET(gtk_builder_get_object(builder,"email"));
-    foodtruck = GTK_WIDGET(gtk_builder_get_object(builder,"foodtruck"));
-    franchise = GTK_WIDGET(gtk_builder_get_object(builder,"franchise"));
-    capital = GTK_WIDGET(gtk_builder_get_object(builder,"capital"));
-    password = GTK_WIDGET(gtk_builder_get_object(builder,"password"));
+    firstname = GTK_WIDGET(gtk_builder_get_object(builder,"firstname"));
+    lastname = GTK_WIDGET(gtk_builder_get_object(builder,"lastname"));
+    phonenumber = GTK_WIDGET(gtk_builder_get_object(builder,"phone"));
+    //password = GTK_WIDGET(gtk_builder_get_object(builder,"password"));
     validation = GTK_WIDGET(gtk_builder_get_object(builder,"validation"));
 
-    g_signal_connect(immatriculation1,"changed",G_CALLBACK(enterImmatriculation1),immatriculation1);
-    g_signal_connect(immatriculation2,"changed",G_CALLBACK(enterImmatriculation2),immatriculation2);
-    g_signal_connect(immatriculation3,"changed",G_CALLBACK(enterImmatriculation3),immatriculation3);
     g_signal_connect(email,"changed",G_CALLBACK(enteremail),email);
-    g_signal_connect(foodtruck,"changed",G_CALLBACK(enterfoodtruck),foodtruck);
-    g_signal_connect(franchise,"changed",G_CALLBACK(enterfranchise),franchise);
-    g_signal_connect(capital,"changed",G_CALLBACK(entercapital),capital);
-    g_signal_connect(password,"changed",G_CALLBACK(enterpassword),password);
+    g_signal_connect(firstname,"changed",G_CALLBACK(enterfirstname),firstname);
+    g_signal_connect(lastname,"changed",G_CALLBACK(enterlastname),lastname);
+    g_signal_connect(phonenumber,"changed",G_CALLBACK(enterphonenumber),phonenumber);
+    //g_signal_connect(password,"changed",G_CALLBACK(enterpassword),password);
     g_signal_connect (validation,"clicked",G_CALLBACK(check),window);
 }
 
 void AccueilDestroy(GtkWidget *widget,GtkWidget *window)
 {
-    gtk_widget_destroy(window);
+    
     builder = gtk_builder_new_from_file("Accueil.glade");
     gtk_builder_connect_signals(builder, NULL);
     window  = GTK_WIDGET(gtk_builder_get_object(builder,"windows"));
@@ -156,75 +133,82 @@ void AccueilDestroy(GtkWidget *widget,GtkWidget *window)
 }
 
 
-void check()
+void check(GtkWidget *widget,GtkWidget *window)
 {
-    gtk_main_quit();
-    MYSQL *con = mysql_init(NULL);
-    if(con == NULL)
-    {
-        fprintf(stderr,"%s\n",mysql_error(con));
+
+    const char *verif;
+    unsigned int i;
+    char verif_email_one ='@';
+    char verif_email_two ='.';
+    bool access_email_one = FALSE;
+    bool access_email_two = FALSE;
+    bool access_phonenumber = TRUE;
+    size_t size_email,size_firstname,size_lastname,size_phonenumber/*,size_password*/;
+
+    size_phonenumber = strlen(phonenumberchar);
+    size_email = strlen(emailchar);
+    size_firstname = strlen(firstnamechar);
+    size_lastname = strlen(lastnamechar);
+   // size_password = strlen(passwordchar);
+
+    verif = emailchar;
+    while((verif= strchr(verif,verif_email_one))!=NULL){
+        ++verif;
+        access_email_one = TRUE;
+    }
+    verif = emailchar;
+    while((verif= strchr(verif,verif_email_two))!=NULL){
+        ++verif;
+        access_email_two = TRUE;
     }
 
-    if(mysql_real_connect(con,"localhost","drivn&cook","drivn&cookpassword","qrcodebase",0,NULL,0)==NULL)
-    {
-        finish_with_error(con);
+    for( i=0; i<size_phonenumber; i++ ) {
+        if(!isdigit(phonenumberchar[i]))
+        {
+            access_phonenumber = FALSE;
+            printf("%d",phonenumberchar[i]);
+            break;
+        }
     }
-
-    char test_mail[] = "SELECT immatriculation FROM franchises WHERE email ='";
-    strcat(test_mail,emailchar);
-    strcat(test_mail,"'");
-
-    if(mysql_query(con,test_mail))
+    if(access_email_one && access_email_two && access_phonenumber && (size_email <=200)&& (size_firstname <=50)&& (size_lastname <=50) /*&&(size_password>=8)&&(size_password<=100)*/&&(size_phonenumber==10))
     {
-        finish_with_error(con);
-    }
 
-    MYSQL_RES * result = mysql_store_result(con);
-
-    if(result == NULL)
-    {
-        finish_with_error(con);
-    }
-    int num_rows = mysql_num_rows(result);
-
-    if(num_rows == 0)
-    {
-        char reqInsert[500] = "INSERT INTO franchises(immatriculation,foodtruck_name,franchises_name,capital,email,password) VALUES('";
-        strcat(reqInsert,immatriculation1char);
-        strcat(reqInsert,"-");
-        strcat(reqInsert,immatriculation2char);
-        strcat(reqInsert,"-");
-        strcat(reqInsert,immatriculation3char);
-        strcat(reqInsert,"','");
-        strcat(reqInsert,foodtruckchar);
-        strcat(reqInsert,"','");
-        strcat(reqInsert,franchisechar);
-        strcat(reqInsert,"',");
-        strcat(reqInsert,capitalchar);
-        strcat(reqInsert,",'");
+        /*BYTE encode[SHA256_BLOCK_SIZE] ;
+        SHA256_CTX ctx;
+        sha256_init(&ctx);
+        sha256_update(&ctx,(BYTE*)passwordchar, size_password);
+        sha256_final(&ctx,encode);
+        for (int i = 0; i < SHA256_BLOCK_SIZE; i++)
+        {
+            char hexa[4];
+            sprintf(hexa, "%d", encode[i]);
+            puts(hexa);
+            strcat(reqInsert, hexa);
+        }*/
+        char reqInsert[2000] = "INSERT INTO franchise(email,role_id,first_name,last_name,phone_number,password) VALUES(";
+        strcat(reqInsert,"'");
         strcat(reqInsert,emailchar);
+        strcat(reqInsert,"',");
+        strcat(reqInsert,"2");
+        strcat(reqInsert,",'");
+        strcat(reqInsert,firstnamechar);
         strcat(reqInsert,"','");
-        strcat(reqInsert,passwordchar);
+        strcat(reqInsert,lastnamechar);
+        strcat(reqInsert,"','");
+        strcat(reqInsert,phonenumberchar);
+        strcat(reqInsert,"','");
+        strcat(reqInsert,"PassToChange");
         strcat(reqInsert,"');");
 
-        if(mysql_query(con,reqInsert))
-        {
-            finish_with_error(con);
-        }
         qrEncode(reqInsert);
-    }else{
-        printf("the immatriculation is already used\n");
+        gtk_widget_destroy(window);
+
     }
-    mysql_close(con);
-
+    else{
+        inscription(widget,window);
+    }
 }
 
-void finish_with_error(MYSQL * con)
-{
-fprintf (stderr, "%s\n", mysql_error (con));
-mysql_close (con);
-exit(1);
-}
 
 void qrEncode(char *insert)
 {
@@ -243,10 +227,10 @@ void printQr(const uint8_t qrcode[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Window *window = NULL;
 
-	window = SDL_CreateWindow("QRcode",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,47,47,0);
+	window = SDL_CreateWindow("QRcode",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,60,60,0);
 	SDL_Rect square;
-	square.h =100;
-	square.w=100;
+	square.h =300;
+	square.w=300;
 	FILE *test;
 	test=fopen("QR-code.txt","w+");
 
@@ -256,7 +240,7 @@ void printQr(const uint8_t qrcode[]) {
 			if(qrcodegen_getModule(qrcode,x,y))
 			{
 			    SDL_Surface *s;
-			    s = SDL_CreateRGBSurface(0,100,100,32,0,0,0,0);
+			    s = SDL_CreateRGBSurface(0,20,20,32,0,0,0,0);
 			    square.x=x+border;
 			    square.y=y+border;
 			    SDL_FillRect(s,NULL,SDL_MapRGB(s->format,0,0,0));
@@ -265,7 +249,7 @@ void printQr(const uint8_t qrcode[]) {
             else
 			{
 			    SDL_Surface *s;
-			    s = SDL_CreateRGBSurface(0,100,100,32,0,0,00,0);
+			    s = SDL_CreateRGBSurface(0,20,20,32,0,0,00,0);
 			    square.x=x+border;
 			    square.y=y+border;
 			    SDL_FillRect(s,NULL,SDL_MapRGB(s->format,255,255,255));
@@ -274,32 +258,53 @@ void printQr(const uint8_t qrcode[]) {
 		}
 		fputs("\n", test);
 	}
+
 	IMG_SavePNG(SDL_GetWindowSurface(window),"essai.png");
 	SDL_DestroyWindow(window);
-
-
-
-
-   CURLcode res;
-   CURL* curl = curl_easy_init();
-   curl_easy_setopt(curl,CURLOPT_URL,"ftps://alex:alex@51.77.158.251:37812");
-   res = curl_easy_perform(curl);
-   if(res != CURLE_OK)
-   {
-       fprintf(stderr, "%s\n",curl_easy_strerror(res));
-   }
-
-
-  curl_easy_cleanup(curl);
-  finish();
+	fclose(test);
+	SDL_Quit();
+	transfert();
 
 }
-int finish()
+
+int transfert()
 {
-    SDL_Quit();
-     return 0;
+    CURL *curl;
+    CURLcode res;
+    FILE *hd_src;
+    struct stat file_info;
+    curl_off_t fsize;
+
+      /* get the file size of the local file */
+    if(stat(LOCAL_FILE, &file_info)) printf("Couldn't open '%s': %s\n", LOCAL_FILE, strerror(errno));
+    fsize = (curl_off_t)file_info.st_size;
+    /* get a FILE * of the same file */
+    hd_src = fopen(LOCAL_FILE, "rb");
+
+    /* In windows, this will init the winsock stuff */
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    if(curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+        curl_easy_setopt(curl, CURLOPT_URL,"ftp://ftp.drivncook.space/essai.png");
+        curl_easy_setopt( curl, CURLOPT_USERPWD, "alex:alex" );
+        curl_easy_setopt(curl, CURLOPT_READDATA, hd_src);
+        curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,(curl_off_t)fsize);
+
+        /* Now run off and do what you've been told! */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if(res != CURLE_OK)fprintf(stderr, "curl_easy_perform() failed: %s\n",curl_easy_strerror(res));
+        else
+        {
+            curl_easy_cleanup(curl);
+            fclose(hd_src); /* close the local file */
+            curl_global_cleanup();
+            return 0;
+        }
+    }
+    return 0;
 }
-
-
 
 
